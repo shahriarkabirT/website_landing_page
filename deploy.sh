9262d2e8ffd1bffc7d206cdbc292b5ac447890ab
Shahriar Kabir <shahriarkabir078@gmail.com>
2026-05-16 20:44:41 +0600

    navbar

---
+++ deploy.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+export PATH=$PATH:/root/.nvm/versions/node/v24.14.1/bin
+
+echo "🚀 Starting deployment..."
+
+cd /var/www/idokan_ecom
+
+echo "📦 Pulling latest code..."
+git pull origin main
+
+# server setup
+
+echo "Enter server folder"
+cd server
+
+echo "📥 Installing dependencies..."
+pnpm install
+
+echo "🔨 Building app..."
+pnpm run build
+
+echo "🔄 Restarting PM2..."
+pm2 restart idokan_server
+
+# client setup
+
+echo "Enter client folder"
+cd client
+
+echo "📦 Pulling latest code..."
+git pull origin main
+
+echo "📥 Installing dependencies..."
+pnpm install
+
+echo "🔨 Building app..."
+pnpm run build
+
+echo "🔄 Restarting PM2..."
+pm2 restart idokan_client
+
+echo "✅ Deployment done!" 
